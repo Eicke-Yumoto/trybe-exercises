a05ba4e6d97985b345056a9b3b795188a3f021bd
Eicke Yumoto <eickeyumoto@gmail.com>
2021-02-24 18:43:29 -0300

    Adiciona primeiras ativivdades de JavaScript

---
+++ bloco_4/dia_1/dia1.0.js
@@ -0,0 +1,14 @@
+const name = "Eicke Yumoto"
+const birthCity = "Balneário Camboriú"
+var birthYear = 2001
+var birthYear = 2020
+
+console.log (name)
+
+console.log (birthCity)
+
+console.log (birthYear)
+
+var birthCity = "Fortal"
+
+
---
+++ bloco_4/dia_1/dia1.2.js
@@ -0,0 +1,6 @@
+let base = 5
+let altura = 8
+let area = base * altura;
+let perimetro = 5 * 2 + 8 * 2;
+
+console.log(perimetro)
