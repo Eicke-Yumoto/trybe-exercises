2f377a5c10d6d02043091143675558753fd32408
Eicke Yumoto <eickeyumoto@gmail.com>
2021-02-24 19:00:25 -0300

    Adiciona exercicio de if

---
+++ bloco_4/dia_1/dia1.3.js
@@ -0,0 +1,10 @@
+let Eicke = 100
+
+if (Eicke >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!" )
+} else if (Eicke < 80 && Eicke >= 60) {
+    console.log("Você está na nossa lista de espera")
+} else {console.log("Você foi reprovada(o)")}
+    
+
+
