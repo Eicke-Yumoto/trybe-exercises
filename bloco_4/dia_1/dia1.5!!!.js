313d9168b5c5f6aa7a88f475c277e77a5107a417
Eicke Yumoto <eickeyumoto@gmail.com>
2021-02-25 16:04:15 -0300

    Adiciona atividades

---
+++ bloco_4/dia_1/dia1.5!!!.js
@@ -0,0 +1,26 @@
+let a = 5
+let b = 10
+let c = 15
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+if(a > b) {
+    console.log(a)
+} else {
+    console.log(b)
+}
+
+if(a > b && a > c) {
+    console.log(a)
+} else if(b > a && b > c) {
+    console.log(b)
+} else {
+    console.log(c)
+}
+
+if()
+    
